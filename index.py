af15a7b59c9adb353ef1e445efa081eb71b5e95f
zwt <1295125020@qq.com>
2020-10-18 21:07:14 +0800

    filter, separateBp and mem has not finshed

--- index.py
+++ index.py
@@ -8,5 +8,18 @@ import mem
 import readRecords
 import separateBP
 
-
+for i in range(10):
+  recordsPath = f'../checkedDir/3{i}.txt'
+  # 1. 读取checkdDir中的文件并将返回的字符串数组记录在records变量中
+  records = readRecords.readRecords(recordsPath)
+  recordsLen = len(records)
+  if recordsLen != 0:
+    # 2. 遍历records数组并调用markSignal函数，并将返回的结果传给filter函数进行滤波处理，处理完成后放入filtedList
+    # filtedList是一个字典数组
+    filtedList = []
+    for recordPath in records:
+      marked = markSignal.markSignal(recordPath)
+      filtedMarked = filter.filter(marked)
+      filtedList.append(filtedMarked)
+    # 将checkedDir下的一个文件全部处理完后将filtedList的存储结果写入filtedData下
 
--- markSignal.py
+++ markSignal.py
@@ -1,6 +1,6 @@
 # 本文件提供函数用来找到ABP和PLETH信号在信号段中的位置
 # 函数参数：recordPath: 需要读取的数据的路径(通过网络读取)
-# 以字典形式返回，字典中有两个键，ＡＢＰ和ＰＬＥＴＨ，对应的值就是在信号段中的位置
+# 以字典形式返回，字典中有两个键，ＡＢＰ和ＰＬＥＴＨ，对应的值就是信号值组成的数组
 
 import wfdb
 
@@ -9,15 +9,17 @@ def markSignal(recordPath):
     recordName = recordPath[11:len(recordPath)]
     pn_dir = f'published-projects/mimic3wdb/1.0/{recordPath[0:11]}'
 
-    record = wfdb.rdheader(recordName, pn_dir=pn_dir)
-    if record != None:
-        sig_names = record.sig_name
-        res = {"ABP": 0, "PLETH": 0}
-        res["ABP"] = sig_names.index("ABP")
-        res["PLETH"] = sig_names.index("PLETH")
+    record = wfdb.rdrecord(recordName, pn_dir=pn_dir)
+    sig_names = record.sig_name
+    res = {"ABP": 0, "PLETH": 0}
+    ABP_idx = sig_names.index("ABP")
+    PLETH_idx = sig_names.index("PLETH")
+    res["ABP"] = record.p_signal[:, ABP_idx]
+    res["PLETH"] = record.p_signal[:, PLETH_idx]
 
-        return res
-    else:
-      return None
+    print(res)
+
+    return res
+
 
 # markSignal('31/3104269/3104269_0011')
